8ebfa267d31a1c0a54396a4735c3a32fe0b12fbc
Redolent7 <mateus.gomes06@escola.pr.gov.br>
2025-08-13 16:07:06 -0300

    Create app.js

---
+++ assets/app.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Rocket League',
+    'Qual o objetivo principal do jogo?',
+    'Marcar gols usando carros turbo.'
+)
+
+criaCartao(
+    'Rocket League',
+    'Quantos jogadores por time no modo padrão?',
+    'Normalmente 3 contra 3.'
+)
+
+criaCartao(
+    'Rocket League',
+    'O que é o "Boost"?',
+    'Recurso que aumenta a velocidade do carro.'
+)
+
+criaCartao(
+    'Rocket League',
+    'Qual a função das "Demolitions"?',
+    'Explodir o carro adversário ao colidir em alta velocidade.'
+)
+
+criaCartao(
+    'Rocket League',
+    'O que é o modo "Rumble"?',
+    'Modo com poderes especiais para cada jogador.'
+)
+
+criaCartao(
+    'Rocket League',
+    'Qual empresa desenvolveu o Rocket League?',
+    'Psyonix.'
+)
+
+criaCartao(
+    'Rocket League',
+    'Qual é a moeda do jogo?',
+    'Credits.'
+)
+
+criaCartao(
+    'Rocket League',
+    'O que é o "Aerial"?',
+    'Movimento para acertar a bola no ar usando o boost.'
+)
+
+criaCartao(
+    'Rocket League',
+    'Qual foi o ano de lançamento?',
+    '2015.'
+)
+
+criaCartao(
+    'Rocket League',
+    'O que são "Ranks"?',
+    'Classificações baseadas no desempenho do jogador.'
+)
